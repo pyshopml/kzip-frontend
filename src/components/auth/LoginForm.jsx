2e6f5ed5c51be3cd982e546b659df68cf64ba1a8
Nikita Luparev <nikita.luparev@gmail.com>
2017-01-05 15:26:00 +0500

    refactored and simplified login form

--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -2,76 +2,40 @@ import React, {createClass} from 'react';
 import {Field, reduxForm} from 'redux-form';
 import {isNil} from 'ramda';
 
+// validations
+const required = value => value ? undefined : 'Необходимо'
+const email = value =>
+  value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ?
+  'Не корректный E-mail' : undefined
+
+
 const renderField = ({ input, label, type, meta: { touched, error, warning } }) => (
-  <div className="input-field">
-    <input {...input} placeholder={label} type={type} className="form-control" />
-    { touched && (error && <span className="error-msg">{error}</span>) }
+  <div className={error ? "form-group has-error" : "form-group"}>
+    <div className="input-field">
+      <input {...input} placeholder={label} type={type} className="form-control" />
+      { touched && (error && <span className="error-msg">{error}</span>) }
+    </div>
   </div>
 );
 
 const LoginForm = createClass({
-  getInitialState() {
-    return {
-      emailValid : true,
-      passwordValid : true
-    };
-  },
-
-  _requiredEmail (val) {
-    if (isNil(val)) {
-      this.setState({ emailValid : false });
-      return 'Необходимо';
-    } 
-
-    this.setState({ emailValid : true });
-    return undefined;
-  },
-
-  _requiredPassword (val) {
-    if (isNil(val)) {
-      this.setState({ passwordValid : false });
-      return 'Необходимо';
-    }     
-
-    this.setState({ passwordValid : true });
-    return undefined;
-  },
-
-  _isValidEmail (val) {
-    if (val && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val)) {
-      this.setState({ emailValid : false });
-      return 'Не корректный E-mail';
-    } 
-
-    this.setState({
-      emailValid : true
-    })
-    return undefined;
-  },
-
   render () {
     let {handleSubmit} = this.props;
-    let {_requiredEmail, _requiredPassword, _isValidEmail} = this;
-    let {emailValid, passwordValid} = this.state;
 
     return (
       <form onSubmit={handleSubmit}>
-        <div className={emailValid ? "form-group" : "form-group has-error"}>
-          <Field 
-            type="email" 
-            component={renderField}
-            name="email" 
-            placeholder="E-mail"
-            validate={[ _requiredEmail, _isValidEmail ]} />
-        </div>
-        <div className={passwordValid ? "form-group" : "form-group has-error"}>
-          <Field 
-            type="password" 
-            component={renderField}
-            name="password" 
-            placeholder="Пароль"
-            validate={_requiredPassword} />
-        </div>
+        <Field 
+          type="email" 
+          component={renderField}
+          name="email" 
+          placeholder="E-mail"
+          validate={[ required, email ]} />
+        <Field 
+          type="password" 
+          component={renderField}
+          name="password" 
+          placeholder="Пароль"
+          validate={required} />
         <button type="submit" className="btn btn-primary">Войти</button>
       </form>  
     );
