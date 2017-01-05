3c0030b0447fea3d50bcfe38858ba453e3f9a6e6
Nikita Luparev <nikita.luparev@gmail.com>
2017-01-05 19:46:29 +0500

    fixed a typo

--- src/utils/form_validations.js
+++ src/utils/form_validations.js
@@ -11,7 +11,7 @@ const withoutSpaces = str =>
   str && /^(\s|\S)+$/i.test(str) ? 'Не должно быть пробелов' : undefined;
 
 const withoutSpecialChars = str => 
-  str && /\W+/i.test(str) ? 'Только буквы, числа, нижнее подчеркивание' : undefined;
+  str && /\W+/i.test(str) ? 'Только буквы, числа и нижнее подчеркивание' : undefined;
 
 const minLength = min => str =>
   str && (str.length < min) ? `Должно быть не меньше ${min} символов` : undefined
