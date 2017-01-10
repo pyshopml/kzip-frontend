fa69369312d194a99ebaa1d024fa135e33132100
Nikita Luparev <nikita.luparev@gmail.com>
2017-01-10 13:45:58 +0500

    implemented basic user's profile page

--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
 
-const Profile = () => (
+const Profile = ({ user }) => (
   <div>
-    <h2>Профиль</h2>
+    <h2>{ user.email }</h2>
   </div>
 );
 
-export default Profile;
+Profile.propTypes = {
+  user: PropTypes.shape().isRequired,
+};
+
+const mapStateToProps = ({ auth }) => ({
+  user: auth.user,
+});
+
+export default connect(mapStateToProps)(Profile);
