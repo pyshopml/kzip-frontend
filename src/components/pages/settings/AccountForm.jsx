58ae070a271ef7d64808c2902f5a34c04eb51297
Nikita Luparev <nikita.luparev@gmail.com>
2017-01-10 14:55:28 +0500

    added accont's form

--- src/components/pages/settings/AccountForm.jsx
+++ src/components/pages/settings/AccountForm.jsx
@@ -2,7 +2,25 @@ import React from 'react';
 
 const AccountForm = () => (
   <div>
-    <h2>Account</h2>
+    <form className="form-horizontal">
+      <div className="form-group">
+        <label for="inputEmail3" className="col-sm-2 control-label">Email</label>
+        <div className="col-sm-10">
+          <input type="email" className="form-control" id="inputEmail3" placeholder="Email" />
+        </div>
+      </div>
+      <div className="form-group">
+        <label for="inputPassword3" className="col-sm-2 control-label">Password</label>
+        <div className="col-sm-10">
+          <input type="password" className="form-control" id="inputPassword3" placeholder="Password" />
+        </div>
+      </div>
+      <div className="form-group">
+        <div className="col-sm-offset-2 col-sm-10">
+          <button type="submit" className="btn btn-default">Sign in</button>
+        </div>
+      </div>
+    </form>
   </div>
 );
 
