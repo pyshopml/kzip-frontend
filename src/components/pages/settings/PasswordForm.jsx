7c74f04b6d65d21adc5caeee4e704be6630c7d57
Nikita Luparev <nikita.luparev@gmail.com>
2017-01-12 22:39:35 +0500

    added on change for input

--- src/components/pages/settings/PasswordForm.jsx
+++ src/components/pages/settings/PasswordForm.jsx
@@ -2,20 +2,42 @@ import React, { Component, PropTypes } from 'react';
 import firebase from '../../../utils/auth';
 
 class PasswordForm extends Component {
-  componentWillMount() {
-    const user = firebase.auth().currentUser;
-    console.log(user);
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      password: '',
+      password_confirmation: '',
+    };
+
+    this.updatePassword = this.updatePassword.bind(this);
+    this.updatePasswordConfirmation = this.updatePasswordConfirmation.bind(this);
+  }
+
+  updatePassword(str) {
+    this.setState({ password: str  });
+  }
+
+  updatePasswordConfirmation() {
+    this.setState({ password_confirmation: str  });
   }
 
   render() {
+    const { password, password_confirmation } = this.state;
+
     return (
       <form>
         <div className="form-group">
-          <input  type="password" className="form-control"  placeholder="Пароль" />
+          <input type="password" onChange={this.updatePassword} className="form-control"  placeholder="Пароль" value={password} />
           <span className="error-msg"></span>
         </div>
         <div className="form-group">
-          <input  type="email"  className="form-control" id="password" placeholder="Пароль ещё раз" />
+          <input 
+            type="password" 
+            onChange={this.updatePasswordConfirmation} 
+            className="form-control" 
+            placeholder="Пароль ещё раз" 
+            value={password_confirmation} />
           <span className="error-msg"></span>
         </div>
         <button className="btn btn-primary">
