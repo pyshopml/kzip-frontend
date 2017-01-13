79c5bd995c4ac2cb9327f47eee1ba39e0f0c8159
Nikita Luparev <nikita.luparev@gmail.com>
2017-01-13 16:00:24 +0500

    made small refactoring in updateProfileReducer

--- src/reducers/updateProfileReducer.js
+++ src/reducers/updateProfileReducer.js
@@ -2,13 +2,12 @@ import * as types from '../actions/actionTypes';
 
 const model = {
   inProgress: false,
-  errorMsg: '',
+  errorMsg: 'hello',
   successMsg: '',
 }
 
-export default (state=model, action) => {
+export default (state = model, action) => {
   switch (action.type) {
-
     case types.UPDATE_PROFILE_STARTED:
       return Object.assign({}, state, { inProgress: true });
 
@@ -16,16 +15,17 @@ export default (state=model, action) => {
       return Object.assign(
               {}, 
               state, 
-              { inProgress: false, errorMsg: '', successMsg: action.msg });
+              model,
+              { successMsg: action.msg });
 
     case types.UPDATE_MESSAGE_HIDE:
       return Object.assign({}, state, { successMsg: '' });
 
     case types.LOCATION_CHANGE: 
-      return Object.assign({}, state, { inProgress: false, successMsg: '', errorMsg: '' });
+      return Object.assign({}, state, model);
 
     case types.FORM_CHANGE:
-      return Object.assign({}, state, { inProgress: false, successMsg: '', errorMsg: '' });
+      return Object.assign({}, state, model);
 
     case types.UPDATE_PROFILE_FAILED: 
       return Object.assign({}, state, { inProgress: false, errorMsg: action.error });
