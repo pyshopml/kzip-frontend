a107f2569a52242a66798e34cdef8898e02cd010
Nikita Luparev <nikita.luparev@gmail.com>
2017-01-14 13:05:06 +0500

    divider actionTypes into groups

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -6,21 +6,27 @@
 * Nick Luparev nikita.luparev@gmail.com
 ------------------------------------------------------------------------------- */
 export const AUTH_CHANGED = 'AUTH_CHANGED';
+
 export const LOGIN_STARTED = 'LOGIN_STARTED';
 export const LOGIN_FINISHED = 'LOGIN_FINISHED';
 export const LOGIN_FAILED = 'LOGIN_FAILED';
+
 export const LOGOUT_STARTED = 'LOGOUT_STARTED';
 export const LOGOUT_FINISHED = 'LOGOUT_FINISHED';
 export const LOGOUT_FAILED = 'LOGOUT_FAILED';
+
 export const SIGNUP_STARTED = 'SIGNUP_STARTED';
 export const SIGNUP_FINISHED = 'SIGNUP_FINISHED';
 export const SIGNUP_FAILED = 'SIGNUP_FAILED';
+
 export const LOCATION_CHANGE = '@@router/LOCATION_CHANGE';
 export const FORM_CHANGE = '@@redux-form/CHANGE';
+
 export const UPDATE_PROFILE_STARTED = 'UPDATE_PROFILE_STARTED';
 export const UPDATE_PROFILE_FAILED = 'UPDATE_PROFILE_FAILED';
 export const UPDATE_PROFILE_FINISHED = 'UPDATE_PROFILE_FINISHED';
 export const UPDATE_MESSAGE_HIDE = 'UPDATE_MESSAGE_HIDE';
+
 export const PASSWORD_UPDATE_STARTED = 'PASSWORD_UPDATE_STARTED';
 export const PASSWORD_UPDATE_FAILED = 'PASSWORD_UPDATE_FAILED';
 export const PASSWORD_UPDATE_FINISHED = 'PASSWORD_UPDATE_FINISHED';
