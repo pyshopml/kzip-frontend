d05a7865bb11988041e6bdc5d1776e7b558d8288
Nikita Luparev <nikita.luparev@gmail.com>
2017-01-16 11:23:46 +0500

    hide button for creating posts unless user logged in

--- src/components/sidebar/Navigation.jsx
+++ src/components/sidebar/Navigation.jsx
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router';
+import { firebase } from '../../utils/auth.js';
 
-const Navigation = () => (
-  <nav className="navigation">
-    <ul>
-      <li>
-        <Link className="link nav-link" to="/posts">
-          <span className="glyphicon glyphicon-book" />
-          Заявки
-        </Link>
-      </li>
-    </ul>
-    <button type="button" className="btn btn-success">
-      <Link to="/newpost">Создать заявку</Link>
-    </button>
-  </nav>
-);
+class Navigation extends Component {
+  createPostButton() {
+    return(
+      <button type="button" className="btn btn-success">
+        <Link to="/newpost">Создать заявку</Link>
+      </button>
+    )
+  }
+
+  createPost() {
+    const user = firebase.auth().currentUser;
+    if (user) return this.createPostButton();
+  }
+
+  render() {
+    return (
+      <nav className="navigation">
+        <ul>
+          <li>
+            <Link className="link nav-link" to="/posts">
+              <span className="glyphicon glyphicon-book" />
+              Заявки
+            </Link>
+          </li>
+        </ul>
+        { this.createPost() }
+      </nav>
+    );
+  }
+  
+};
 
 export default Navigation;
