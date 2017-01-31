26fcf1a773528727ea49b9b4547773272da461c2
Nikita Luparev <nikita.luparev@gmail.com>
2017-02-01 01:21:44 +0500

    fixed issue with properly display link to user's profile

--- src/components/UserMenu/index.js
+++ src/components/UserMenu/index.js
@@ -6,7 +6,9 @@ import css from './style.scss';
 const UserMenu = ({ user, expandPanel }) => (
   <div className={css.user_menu_controls}>
     <span className="glyphicon glyphicon-user" />
-    <Link to="profile" className="link">{ user.displayName || user.email }</Link>
+    <Link to="profile" className={css.link}>
+      { user.displayName || user.email }
+    </Link>
     <button 
       onClick={ expandPanel } 
       className={ `glyphicon glyphicon-menu-down ${css.expand_control}` } />
